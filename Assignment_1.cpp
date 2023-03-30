fedc077ef3fcc0741ebdd103c9377c4f53756477
Poorav SHARMA <30045900@tafe.wa.edu.au>
2023-03-31 03:25:10 +0800

    Version5

--- Assignment_1.cpp
+++ Assignment_1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #include <iostream>
 #include <vector>
 #include <string>
+#include <sstream>
 
 struct Word {
 	string name;
@@ -14,31 +15,36 @@ void searchWord();
 void threeZZZFinder();
 void addWord();
 void inputCheck(int value);
+string capitalRemover(string word);
+string typeConverter(string type);
+string definitionSeperator(string definition);
 vector<Word> Dictionary;
+bool loadedDictionary = false;
 int main()
 {
 
 	string dictionarynameInput;
 	cout << "Hi!! Input the name for the dictionary you want to load." << endl;
 	string dictionaryName = "dictionary_2023S1.txt";
-	bool loadedDictionary = false;
+	
 	while (loadedDictionary == false) {
 		cin >> dictionarynameInput;
-		if (dictionaryName.compare(dictionarynameInput) == 0) {
+		if (dictionaryName.compare(dictionarynameInput) == 0 || dictionarynameInput.length() >= 4 && dictionarynameInput.substr(dictionarynameInput.length() - 4) == ".txt") {
 			cout << "Loading dictionary....." << endl << endl;
 			TextFileReader(dictionarynameInput);
+			
 		}
 		else {
-			cout << "Dictionary not loaded" << endl << "Incorrect Dictionary name!!!" << endl << "Please try again!!!" << endl;
+			cout << "No Dictionary loaded" << endl << "Please try again!!!" << endl;
 
 		}
 	}
-	cout << "Dictionary has been loaded";
-	bool endLoop = false;
+	
+	bool loopingMenu = true;
 	int inputOption;
-	while (endLoop == false) {
-		cout << "Enter '1' to search for a work in the dictionary" << endl;
-		cout << "Enter '2' to print all the works from the dictonary that contain more than three 'z' characters" << endl;
+	while (loopingMenu) {
+		cout << "Enter '1' to search for a word in the dictionary" << endl;
+		cout << "Enter '2' to print all the words from the dictonary that contains more than three 'z' letters" << endl;
 		cout << "Enter '3' to add a word to the dictionary" << endl;
 		cout << "Enter '4' to Exit program" << endl;
 
@@ -46,36 +52,48 @@ int main()
 		inputCheck(inputOption);
 
 		switch (inputOption) {
-		case 1: searchWord();
-		case 2:threeZZZFinder();
-		case 3: addWord();
-		case 4: break;
+		case 1: 
+			searchWord();
+			break;
+		case 2:
+			threeZZZFinder();
+			break;
+		case 3: 
+			addWord();
+			break;
+		case 4:
+			loopingMenu = false;
+			break;
 		}
 
 	}
 
 }
 void TextFileReader(string fileName) {
-	string wordLine;
-
+	
+	cout << "Wait few seconds" << endl;
 	try {
-		ifstream readFile(fileName);
-		int count = 0;
+		string wordLine;
+		ifstream readFile;
+		int count = 1;
+		readFile.open(fileName);
 		if (readFile.is_open()) {
 			while (!readFile.eof()) {
 
 				getline(readFile, wordLine);
 				size_t startWord = wordLine.find("<word>");
+
 				if (startWord != string::npos) {
 					Word input;
 
 					getline(readFile, wordLine);
 					input.name = wordLine;
-					getline(readFile, wordLine);
-					input.type = wordLine;
+
 					getline(readFile, wordLine);
 					input.definition = wordLine;
 
+					getline(readFile, wordLine);
+					input.type = wordLine;
 
 					Dictionary.push_back(input);
 					cout << count << endl;
@@ -83,37 +101,107 @@ void TextFileReader(string fileName) {
 				}
 			}
 			readFile.close();
+			loadedDictionary = true;
+			cout << "Dictionary has been loaded" << endl << endl;
 		}
 		else {
-			cout << "unable to open file" << endl;
+			cout << "unable to open file" << endl<<"Try Again!"<<endl;
 		}
 
 	}
 	catch (exception ex) {
-		cout << "caught an exception" << endl;
+		cout << "Something went wrong!!!" <<ex.what()<< endl;
 	}
 
 }
 void inputCheck(int input) {
 	bool correctInput = false;
+
 	while (correctInput == false) {
-		if (cin >> input || input < 5 || input >0) {
+
+		if (input < 5 && input >0) {
 			correctInput = true;
 
 		}
 		else {
-			cout << "Please input the correct number corrosponding to the option you want to choose" << endl;
+			cout << "Please input the correct number corrosponding to the option you want to choose!!!" << endl<<endl;
 			cin.clear();
 			cin.ignore();
+			cin >> input;
 		}
 	}
 }
 void searchWord() {
+	string searchedWord;
+
+	cout <<endl<<endl<< "What word are your searching for?"<<endl;
+	cin >> searchedWord;
+	searchedWord = capitalRemover(searchedWord);
+	bool  foundWord = false;
+	for(auto word : Dictionary) {
+		if (word.name.compare(searchedWord) == 0) {
+			string type = typeConverter(word.type);
+			string definition = definitionSeperator(word.definition);
+			cout << endl <<"Search Result: "<<endl<< "Word: " << word.name << endl << "Type: " << type << endl << "Definitions: " <<endl<< definition << endl << endl;
+			foundWord = true;
+			break;
+		}
+		
+	}
+	if (foundWord == false) {
+	
+		cout << endl << "Word '" << searchedWord << "' cound not be found in the dictionary" << endl << endl;
+	
+	}
 
 }
 void threeZZZFinder() {
-
+	cout << "threeZZZFinder" << endl;
 }
 void addWord() {
+	cout << "addWord" << endl;
+}
+string capitalRemover(string word) {
+	string lowerWord;
+	for (char w : word) {
+		lowerWord += tolower(w);
+	}
+	return lowerWord;
+}
+string typeConverter(string type) {
+	if (type == "n") {
+		type = "[name]";
+	}
+	else if(type == "v") {
+		type = "[verb]";
+	}
+	else if (type == "adv") {
+		type = "[adverb]";
+	}
+	else if (type == "adj") {
+		type = "[adjective]";
+	}
+	else if (type == "prep") {
+		type = "[preposition]";
+	}
+	else if (type == "misc") {
+		type = "[miscellaneous]";
+	}
+	else if (type == "pn") {
+		type = "[proper noun]";
+	}
+	else {
+		type = "[noun and verb]";
+	}
+	return type;
+}
+string definitionSeperator(string definition) {
+	istringstream iss(definition);
+	string meanings;
+	string newDefinition;
+	while (getline(iss, meanings, ';')) {		
+			newDefinition += "- " + meanings + "\n";
+	}
+	return newDefinition;
 
 }
